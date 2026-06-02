5f10c5fdbd3e2399a0380d2924ccac4c98fc3a00
daviddingn-sys <david.dingn@gmail.com>
2026-06-02 21:41:24 +0800

    Add family invitation acceptance

---
+++ src/app/(app)/invitations/page.tsx
@@ -0,0 +1,7 @@
+import { InvitationsClient } from "@/components/invitation/InvitationsClient";
+
+export const dynamic = "force-dynamic";
+
+export default function InvitationsPage() {
+  return <InvitationsClient />;
+}
