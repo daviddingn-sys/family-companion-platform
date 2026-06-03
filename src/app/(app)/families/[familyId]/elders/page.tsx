66463d318fcd2277018d2f0b6b076f1fe149132a
daviddingn-sys <david.dingn@gmail.com>
2026-06-03 14:17:39 +0800

    Align family management permissions in pages

--- src/app/(app)/families/[familyId]/elders/page.tsx
+++ src/app/(app)/families/[familyId]/elders/page.tsx
@@ -1,4 +1,7 @@
+import { notFound } from "next/navigation";
 import { EldersClient } from "@/components/elder/EldersClient";
+import { requireUser } from "@/lib/auth";
+import { getFamilyMembership } from "@/lib/permissions";
 
 export default async function EldersPage({
   params,
@@ -6,5 +9,9 @@ export default async function EldersPage({
   params: Promise<{ familyId: string }>;
 }) {
   const { familyId } = await params;
+  const user = await requireUser();
+  const membership = await getFamilyMembership(familyId, user.id);
+  if (!membership) notFound();
+
   return <EldersClient familyId={familyId} />;
 }
